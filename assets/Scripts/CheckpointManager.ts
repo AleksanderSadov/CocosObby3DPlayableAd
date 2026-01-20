3bf68e2d21727177f1d3bc3090fa003d7db3e38f
Aleksander Sadov <aleksander.sadov.gamedev@gmail.com>
2026-01-21 05:10:28 +0700

    Не добавлять офсет по высоте для респавна по начальной позиции игры, офсет нужен только для чекпоинта

--- assets/Scripts/CheckpointManager.ts
+++ assets/Scripts/CheckpointManager.ts
@@ -26,10 +26,10 @@ export class CheckpointManager extends Component {
         let respawnPosition: Vec3;
         if (isValid(this._checkpoint, true)) {
             respawnPosition = this._checkpoint.node.worldPosition.clone();
+            respawnPosition.add(new Vec3(0, this.spawnOffsetY, 0));
         } else {
             respawnPosition = event.defaultSpawn.clone();
         }
-        respawnPosition.add(new Vec3(0, this.spawnOffsetY, 0));
         characterController.centerWorldPosition = respawnPosition;
     }
 
